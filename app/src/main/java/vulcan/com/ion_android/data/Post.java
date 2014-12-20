0c99c6f657343c9836f7a1edcd9f31e0da04259e
Jay Lorenzo <cxplanet@hotmail.com>
2014-12-19 17:11:19 -0800

    Sync up volley changes

---
+++ app/src/main/java/vulcan/com/ion_android/data/Post.java
@@ -0,0 +1,13 @@
+package vulcan.com.ion_android.data;
+
+/**
+ * Created by jayl on 12/19/14.
+ */
+public class Post
+{
+    public String mTitle;
+    public String mThumbnailUrl;
+    public String mDesc;
+    public String mComments;
+
+}
---
+++ app/src/main/java/vulcan/com/ion_android/data/PostListAdapter.java
@@ -0,0 +1,72 @@
+package vulcan.com.ion_android.data;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+import vulcan.com.ion_android.R;
+import vulcan.com.ion_android.net.SessionMgr;
+
+public class PostListAdapter extends BaseAdapter {
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Post> postItems;
+    ImageLoader imageLoader = SessionMgr.getInstance().mImageLoader;
+
+    public PostListAdapter(Activity activity, List<Post> postItems) {
+        this.activity = activity;
+        this.postItems = postItems;
+    }
+
+    @Override
+    public int getCount() {
+        return postItems.size();
+    }
+
+    @Override
+    public Object getItem(int location) {
+        return postItems.get(location);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.post_row, null);
+
+        if (imageLoader == null)
+            imageLoader = SessionMgr.getInstance().mImageLoader;
+        NetworkImageView thumbNail = (NetworkImageView) convertView
+                .findViewById(R.id.thumbnail);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView desc = (TextView) convertView.findViewById(R.id.desc);
+        TextView comments = (TextView) convertView.findViewById(R.id.comments);
+
+        Post p = postItems.get(position);
+
+        thumbNail.setImageUrl(p., imageLoader);
+        title.setText(p.mTitle);
+        desc.setText(p.mDesc);
+        comments.setText(p.mComments);
+
+        return convertView;
+    }
+
+}
